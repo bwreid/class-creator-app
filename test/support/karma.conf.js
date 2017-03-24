72a8dc507e50dab6bb4e6bebf56bf89a1265ac11
Wes Reid <bwreid@gmail.com>
2017-03-24 15:52:39 -0700

    Update dependencies and karma config

--- test/support/karma.conf.js
+++ test/support/karma.conf.js
@@ -1,51 +1,22 @@
-// karma.conf.js
-module.exports = function(config) {
+module.exports = function (config) {
+  var dependencies = [
+    'chai',
+    'angular',
+    'angular-mocks',
+    'angular-route'
+  ]
+  var files = dependencies.map(dep => `node_modules/${dep}/${dep}.js`)
+
   config.set({
-
     basePath: '../../',
-
-    frameworks: ['mocha', 'sinon-chai'],
-
+    frameworks: ['mocha'],
     browsers : ['Chrome'],
-
     reporters : ['mocha'],
-
-    preprocessors: {
-      '**/*.html': ['ng-html2js']
-    },
-
-    files: [
-
-      { pattern: 'node_modules/chai/chai.js', watched: false },
-
-      { pattern: 'node_modules/angular/angular.js', watched: false },
-
-      { pattern: 'node_modules/angular-mocks/angular-mocks.js', watched: false },
-
-      { pattern: 'node_modules/angular-route/angular-route.js', watched: false },
-
+    files: files.concat([
       'app.js',
-
       'controllers/*.js',
-
-      'index.html',
-
       'test/support/setup.js',
-
       'test/**/*.spec.js',
-
-    ],
-
-    // see https://github.com/karma-runner/karma-ng-html2js-preprocessor
-    ngHtml2JsPreprocessor: {
-      moduleName: 'ClassCreatorApp'
-    },
-
-    client: {
-      chai: {
-        includeStack: true
-      }
-    }
-
-  });
-};
+    ])
+  })
+}
